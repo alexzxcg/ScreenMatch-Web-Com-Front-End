ef7d506b6b0718ff8da5c78da1b412f72c7122ce
Alex <alex-xp123@hotmail.com>
2024-10-09 15:23:28 -0300

    Implementado metodo para buscar serie por id

--- java-screenmatch-com-jpa-main/src/main/java/br/com/alura/screenmatch/service/SerieService.java
+++ java-screenmatch-com-jpa-main/src/main/java/br/com/alura/screenmatch/service/SerieService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -34,5 +35,14 @@ public class SerieService {
                         s.getAvaliacao(), s.getGenero(), s.getAtores(),
                         s.getPoster(), s.getSinopse()))
                 .collect(Collectors.toList());
+    }
+
+    public SerieDTO obterPorId(Long id) {
+        Optional<Serie> serie = repositorio.findById(id);
+        if (serie.isPresent()) {
+            Serie s = serie.get();
+            return new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse());
+        }
+        return null;
     }
 }
